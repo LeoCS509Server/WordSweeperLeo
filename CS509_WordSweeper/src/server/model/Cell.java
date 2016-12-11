04f9c28448628da163d5c84fa08cd352a0168a3e
TaraZhu <61420592@qq.com>
2016-12-11 00:35:38 -0500

    ADD Cell Class Javadoc

--- CS509_WordSweeper/src/server/model/Cell.java
+++ CS509_WordSweeper/src/server/model/Cell.java
@@ -1,16 +1,26 @@
 package server.model;
 
-
-
 import util.Letters;
 import util.Location;
 
 
+/**
+ * 
+ * A cell is located at a specific (x,y) coordinate in board. It has letter inside. 
+ * 
+ * This class defines the shared behavior for extensions.
+ * 
+ * @author Tongge Zhu
+ */
+
 public class Cell {
 	
+	/** The location of Cell in the board. */
 	Location location;
+	/** The letter within the cell */
 	String letter;
-	Boolean seleted; //Selected: true, otherwise: off
+	/**The selected cell is the bonus cell. */
+	Boolean seleted;
 	
 	public Cell(Location location) {
 		super();
@@ -19,48 +29,56 @@ public class Cell {
 		this.seleted = false;
 	}
 
-
+	/** Get the location of Cell in the board. */
 	public Location getPosition(){
 		return this.location;
 	}
 	
-	
+	/** Check if the cell has letter. */
 	public boolean hasLetter(){
 		if(this.letter!=null) return true;
 		return false;
 	}
 	
-	
-	//http://stackoverflow.com/questions/2149914/randomly-generate-letters-according-to-their-frequency-of-use
+	/** Assign random letter to the cell. */
 	public void setLetter(){
 		this.letter= Letters.chooseLetter();
 	}
 	
-	public void setLetter(Cell c){///////
+	/** Assign the letter of another cell to the cell.
+	 * @param Cell
+	 */
+	public void setLetter(Cell c){
 		this.letter = c.getLetter();		
 	}
 	
-	
+	/** Return the letter of the cell. */
 	public String getLetter(){
 		return this.letter;
 	}
 	
+	/** Remover the letter of the cell. */
 	public void removeLetter(){
 		this.letter = null;	
 	}
 	
+	/** Check if the cell is the Bonus cell. */
 	public boolean isSeleted(){
 		return this.seleted;
 	}
 	
+	/** Set the cell the Bonus cell. */
 	public void setSeleted(){
 		this.seleted = true;
 	}
 	
-	public void changeLocation(int c, int r){
-		this.location.setColumn(c);
-		this.location.setRow(r);
-	}
+//	/** Assign the letter of another cell to the cell.
+//	 * @param Cell
+//	 */
+//	public void changeLocation(int c, int r){
+//		this.location.setColumn(c);
+//		this.location.setRow(r);
+//	}
 
 	
 }
