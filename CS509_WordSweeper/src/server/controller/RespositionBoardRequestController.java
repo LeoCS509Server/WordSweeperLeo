3ab4dae4cd219495b7c66e214fb64d7805d30f5c
echomelodylwh <wli6@wpi.edu>
2016-12-10 19:16:59 -0500

    Update RespositionBoardRequestController.java

--- CS509_WordSweeper/src/server/controller/RespositionBoardRequestController.java
+++ CS509_WordSweeper/src/server/controller/RespositionBoardRequestController.java
@@ -18,29 +18,43 @@ public class RespositionBoardRequestController {
 		this.model = model;
 	}
 	public Message process(ClientState client, Message request) {
+
 		Node repositionBoardRequest = request.contents.getFirstChild();
 		NamedNodeMap map = repositionBoardRequest.getAttributes();
 		String ID = map.getNamedItem("gameId").getNodeValue();
 		Game game = model.getGame(ID);
-		String name = map.getNamedItem("name").getNodeValue();
-		
+		model.selectGame(ID);
+		String playername = map.getNamedItem("name").getNodeValue();
+		System.out.println(playername);
+		Player pl=game.getPlayer(playername);
 		String rc = map.getNamedItem("rowChange").getNodeValue();
 		String cc = map.getNamedItem("colChange").getNodeValue();
 		int rowchange = Integer.valueOf(rc).intValue();
 		int colchange = Integer.valueOf(cc).intValue();
-		Location location = game.rePosition(game.getPlayer(name), colchange, rowchange);
+		game.rePosition(pl, colchange, rowchange);
+		//game.getPlayer(name).setPlayerLocation(location);////////////
+//		BoardResponseBuilder builder = new BoardResponseBuilder(model);
+//		//construct xml response message
+//		String xmlString = Message.responseHeader(request.id())+builder.build();
+//		Message message = new Message(xmlString);
+//		return message;
+//	
+		//construct xml response message
+		
 		
 		String player = new String();
 		ArrayList<Player> Players = game.getPlayers();
 		for (Player p : Players){
-			player = player + "<player name='" + p.getName() + "' lcoation = '"+p.getPlayerLocation().getColumn()+","+ p.getPlayerLocation().getRow() +"' board = '"+ game.getPlayerboard(p) +"' score='" + p.getScore() +"'/>" ;
+			player = player + "<player name='" + p.getName() + "' position = '"+p.getPlayerLocation().getColumn()+","+ p.getPlayerLocation().getRow() +"' board = '"+ game.getPlayerboard(p) +"' score='" + p.getScore() +"'/>" ;
 		}
 		
 		String xmlString = Message.responseHeader(request.id()) +
-				"<boardResponse "+ player +" gameId='"+ game.getGameID() +"' managingUser = '"+ game.getManageUsername()+"' bonus = '" +location.getColumn() +","+ location.getRow() +"' >" +
-			  "</boardResponse>" +
+				"<boardResponse gameId='"+ game.getGameID() +"' managingUser = '"+ game.getManageUsername()+"' bonus ='" + game.getBoard().getBonusCell().getColumn()+","+ game.getBoard().getBonusCell().getRow()+"'>" +
+			  player +
+				"</boardResponse>" +
 			"</response>";
 		// send this response back to the client which sent us the request.
 		return new Message (xmlString);
+		
 	}
 }
