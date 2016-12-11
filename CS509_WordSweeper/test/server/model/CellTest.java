8d48a86585711c94b94a6c9e9feb74daadbbaca6
TaraZhu <61420592@qq.com>
2016-12-10 21:45:16 -0500

    Add CellTest

---
+++ CS509_WordSweeper/test/server/model/CellTest.java
@@ -0,0 +1,66 @@
+package server.model;
+
+import junit.framework.TestCase;
+import util.Location;
+
+public class CellTest extends TestCase {
+	public void testGetPosition() {
+		Location loc2 = new Location(2,4);
+		
+		Cell c =new Cell(loc2);
+		assertEquals(loc2,c.getPosition());
+	}
+
+	public void testHasLetter() {
+		Location loc2 = new Location(2,4);
+		
+		Cell c =new Cell(loc2);
+		assertEquals(true, c.hasLetter());
+	}
+
+	public void testSetLetter1() {
+		Location loc2 = new Location(2,4);		
+		Cell c =new Cell(loc2);
+		
+		Location loc3 = new Location(3,4);		
+		Cell c2 =new Cell(loc3);
+		
+		c.setLetter(c2);
+		assertEquals(c2.getLetter(), c.getLetter());
+		}
+
+	public void testSetLetterCell() {
+		Location loc2 = new Location(2,4);		
+		Cell c =new Cell(loc2);
+		c.removeLetter();
+		c.setLetter();
+		assertNotNull(c.hasLetter());	
+	}
+
+	public void testGetLetter() {
+		Location loc2 = new Location(2,4);		
+		Cell c =new Cell(loc2);
+		assertNotNull(c.getLetter());		
+	}
+
+	public void testRemoveLetter() {
+		Location loc2 = new Location(2,4);		
+		Cell c =new Cell(loc2);
+		c.removeLetter();
+		assertNull(c.getLetter());
+	}
+
+	public void testIsSeleted() {
+		Location loc2 = new Location(2,4);		
+		Cell c =new Cell(loc2);
+		assertEquals(false, c.isSeleted());
+	}
+
+	public void testSetSeleted() {
+		Location loc2 = new Location(2,4);		
+		Cell c =new Cell(loc2);
+		c.setSeleted();
+		assertEquals(true, c.isSeleted());
+	}
+
+}
