7f7f5df702dfe08ef76a421bad2acef5520001da
TaraZhu <TaraZhu@users.noreply.github.com>
2016-12-11 20:38:39 -0500

    Update BoardTest.java

--- CS509_WordSweeper/test/server/model/BoardTest.java
+++ CS509_WordSweeper/test/server/model/BoardTest.java
@@ -1,91 +1,96 @@
+
 package server.model;
 
-import java.util.List;
 import java.util.ArrayList;
 
 import junit.framework.TestCase;
 import util.Location;
-import util.Key;
 
-public class BoardTest extends TestCase {
+public class BoardTest2 extends TestCase {
 
 	Board board;
 	Board board1;
+	
 	protected void setUp() throws Exception {
 		super.setUp();
 		board = new Board();
-		board1 = new Board(9);
-	}
-
-	protected void tearDown() throws Exception {
-		super.tearDown();
+		board1 = new Board(7);
 	}
 
 
-
-	public void testResetBoardInt() {
-		board = board.resetBoard();
-		board1 = board1.resetBoard(9);
-	}
-	
 	public void testGetSize() {
 		assertEquals(7,board.getSize());
 		assertEquals(9,board1.getSize());
 	}
+
+	public void testResetBoard() {
+		fail("Not yet implemented");
+	}
+
+	public void testResetBoardInt() {
+		fail("Not yet implemented");
+	}
+
 	public void testGetCellContains() {
 		for(int c =1; c <= board.getSize(); c++){
 			for(int r=1; r <= board.getSize(); r++){
-				Key key = new Key(r,c);
-				assertNotNull(board.table.get(key));
-				Location l = new Location(c,r);
+				Location l = new Location(r,c);
+				assertNotNull(board.board.get(l));
 				String contain = board.getCellContains(l);
 				System.out.println(contain);
 				assertFalse(contain.isEmpty());
+				}
 			}
-		}
 	}
+
+	public void testBonusCell() {
+		fail("Not yet implemented");
+	}
+
 	public void testGetBonusCell() {
-		assertNotNull(board.getBonusCell());
-		Location l = board.getBonusCell();
-		System.out.println(l.getRow()+","+l.getColumn());
-	}	
-	
-	public void testRemoveWord(){
-		Location l = new Location(1,1);
-		Key key = new Key(1,1);
-		Cell c = board.table.get(key);
+		fail("Not yet implemented");
+	}
+
+	public void testRemoveWord() {
+		Location l1 = new Location(1,1);
+		Location l2 = new Location(1,2);
+		String letter = board.board.get(l1).getLetter();
+		System.out.println(letter);
 		ArrayList<Location> locations = new ArrayList<Location>();
-		locations.add(l);
+		locations.add(l1);
+		locations.add(l2);
 		board.removeWord(locations);
-		String letter = board.table.get(key).letter;
+		letter = board.board.get(l1).getLetter();
 		System.out.println(letter);
 		assertEquals(null,letter);		
 	}
-	
+
 	public void testRefreshBoard() {
-		Key k2 = new Key(2,1);
-		Key k3 = new Key(3,1);
-		Key k4 = new Key(4,1);
-		Key k5 = new Key(5,1);
-		Key k6 = new Key(6,1);
-		Key k7 = new Key(7,1);
-		board.table.get(k2).removeLetter();
-		board.table.get(k3).removeLetter();
-		String letter4 = board.table.get(k4).letter;
-		String letter5 = board.table.get(k5).letter;
-		String letter6 = board.table.get(k6).letter;
-		String letter7 = board.table.get(k7).letter;
-		System.out.println(letter4+letter5+letter6+letter7);
-		board.refreshBoard();
-		assertEquals(letter4,board.table.get(k2).letter);
-		assertEquals(letter5,board.table.get(k3).letter);
-		assertEquals(letter6,board.table.get(k4).letter);
-		assertEquals(letter7,board.table.get(k5).letter);
-		assertFalse(board.table.get(k6).letter.isEmpty());
-		assertFalse(board.table.get(k7).letter.isEmpty());
-		System.out.println(board.table.get(k2).letter+board.table.get(k3).letter+board.table.get(k4).letter+board.table.get(k5).letter
-				+board.table.get(k6).letter+board.table.get(k7).letter);
+		Location l1 = new Location(1,1);
+		Location l2 = new Location(2,2);
+		Location l3 = new Location(3,3);
+		Location l4 = new Location(4,4);
+		Location l5 = new Location(5,5);
+		Location l6 = new Location(6,6);
+		Location l7 = new Location(7,7);
+		
+		ArrayList<Location> locations = new ArrayList<Location>();
+		locations.add(l1);
+		locations.add(l2);
+		locations.add(l3);
+		locations.add(l4);
+		locations.add(l5);
+		locations.add(l6);
+		locations.add(l7);
+		
+		System.out.println(board1.getBoardContains().length());
+		String s1 =board1.getBoardContains();
+		System.out.println(s1);
+		board1.removeWord(locations);
+		board1.refreshBoard();
+		System.out.println(board1.getBoardContains().length());
+		String s2 =board1.getBoardContains();
+		System.out.println(s2);
+		assertFalse(s1.equals(s2));	
 	}
-
-
 }
