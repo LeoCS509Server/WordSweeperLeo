c9d19f651e2d618aeff6014b1016b3c9e6ed747d
echomelodylwh <wli6@wpi.edu>
2016-12-13 11:11:13 -0500

    Update Game.java

--- CS509_WordSweeper/src/admin/adminmodel/Game.java
+++ CS509_WordSweeper/src/admin/adminmodel/Game.java
@@ -2,8 +2,14 @@ package admin.adminmodel;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-
 import util.ContentsIterator;
+
+/**
+ *Game entity class, which contains all the info and functions about the game.  
+ * 
+ * @author Weihao Li,Zetian Wang (Authors contribute equally)
+ *
+ */
 
 public class Game {
 	String gameid;
@@ -16,6 +22,7 @@ public class Game {
 	 ArrayList<String> playerid;
 	 ArrayList<Integer> score;
 	
+	/**Game constructor*/
 	public Game(String id, int size){  
 		this.gameid = id;
 		this.size = size;
@@ -24,19 +31,36 @@ public class Game {
 		playerid =new ArrayList<String>();
 		score = new ArrayList<Integer>();		
 	}
-
+	
+	/**
+	 * the setter for playerlocation attribute.
+	 * @param playerlocation
+	 */
 	public void setPlayerlocation(int location){
 		this.playerlocation.add(location);
 	}
 	
+	/**
+	 * the setter for playerid attribute.
+	 * @param playerid
+	 */
 	public void setPlayerid(String id){
 		this.playerid.add(id);
 	}
 	
+	/**
+	 * the setter for score attribute.
+	 * @param score
+	 */
 	public void setScore(int score){
 		this.score.add(score);
 	}
 	
+	/**
+	 * the setter for board attribute.
+	 * @param letters
+	 * @param board size
+	 */
 	public void setBoard(String string,int size){
 		String[] boardc = string.split(",");
 		this.size=size;
@@ -46,37 +70,58 @@ public class Game {
 			globalboard[i/size][i%size] = (String) it.next();
 			i++;
 		}
-		/**
-		for(int i=0;i<size;i++){
-			for(int j=0;j<size;j++){
-				globalboard[i][j]=string.substring(k,k+1);
-				k++;
-			}
-		}**/
-	
-	
 	}
 	
+	/**
+	 * the getter for playerlocation attribute.
+	 * 
+	 * @return
+	 */
 	public ArrayList<Integer> getPlayerlocation(){
 		return playerlocation;
 	}
 	
+	/**
+	 * the getter for playerid attribute.
+	 * 
+	 * @return
+	 */
 	public ArrayList<String> getPlayerid(){
 		return playerid;
 	}
 	
+	/**
+	 * the getter for score attribute.
+	 * 
+	 * @return
+	 */
 	public ArrayList<Integer> getScore(){
 		return score;
 	}
 	
+	/**
+	 * the getter for size attribute.
+	 * 
+	 * @return
+	 */
 	public int getSize(){
 		return size;
 	}
 	
+	/**
+	 * the getter for globalboard attribute.
+	 * 
+	 * @return
+	 */
 	public String[][] getGlobalboard(){
 		return globalboard;
 	}
-
+	
+	/**
+	 * the getter for gameid attribute.
+	 * 
+	 * @return
+	 */
 	public String getGameid(){
 		return gameid;
 	}
